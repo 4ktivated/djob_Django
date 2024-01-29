ebca547bd4f883b6e81e376446db3504d60878a9
ruslan_rulb <320fps@gmail.com>
2024-01-29 20:11:49 +0500

    includ urls

--- djob/djob/urls.py
+++ djob/djob/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 from django import views
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from job_parser import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.ExampleView.as_view(template_name='some.html')),
+    path('', include('job_parser.urls'))
 ]
