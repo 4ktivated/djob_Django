c07ddf6dcecbcccbe8cc65290b66d1a9e278b034
ruslan_rulb <320fps@gmail.com>
2024-01-30 20:36:28 +0500

    try make models

--- djob/job_parser/models.py
+++ djob/job_parser/models.py
@@ -1,3 +1,19 @@
+
 from django.db import models
 
-# Create your models here.
+class Vacancies(models):
+    lang = models.CharField(max_length=16)
+    title = models.CharField(max_length=255)
+    company = models.CharField(max_length=255)
+    url = models.URLField()
+    salary = models.CharField(max_length=120)
+    info = models.CharField(max_length=255)
+
+
+'''
+не знаю нужно ли вообще делать отдельную таблицу под иконки сайтов
+с которых берутся вакансии 
+'''
+class Site_images(models):
+    icon = models.ImageField()
+    site_id = models.CharField(max_length=24)
